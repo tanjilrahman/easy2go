2bc5563c1be77481b3692849ef566271fb1a0820
tanjilrahman <tanjil.rahman739@gmail.com>
2026-05-05 20:52:53 +0600

    refactor: update BrandLogo component dimensions and sizes for consistency

--- src/components/brand-logo.tsx
+++ src/components/brand-logo.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export function BrandLogo() {
   return (
-    <div className="relative h-[33px] w-[96px] sm:h-[41px] sm:w-[118px]">
+    <div className="relative h-[33px] w-[78px] sm:h-[41px] sm:w-[96px]">
       <Image
         src="/logo.png"
         alt="Easy2Go"
         fill
         priority
-        sizes="(min-width: 640px) 118px, 96px"
+        sizes="(min-width: 640px) 96px, 78px"
         className="object-contain"
       />
     </div>
